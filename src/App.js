1fce744ff0a5d6d47ca1ac06429891f77235e410
unknown <oyeleyeoyedayo@gmail.com>
2021-12-24 22:53:01 +0000

    add instagram image

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hello</h1>
+      <div className="app_header">
+        <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        alt=""
+        />
+      </div>
+
     </div>
   );
 }
