703f917f16661cbb4b1dc5dd38717db7ef8303e3
unknown <oyeleyeoyedayo@gmail.com>
2021-12-26 11:17:21 +0000

    add multiple post components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Post from "./components/post/Post"
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
       <div className="app_header">
-        <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-        alt=""
+        <img
+          className="app_headerImage" 
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
         />
       </div>
-
+      <h1>Hello</h1>
+      {<Post/>}
+      {<Post/>}
+      {<Post/>}
     </div>
   );
 }
